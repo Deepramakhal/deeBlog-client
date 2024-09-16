402bcd9ac378c70f7c34c8015898605c01b11ce3
Deepramakhal <deepra313131@gmail.com>
2024-09-16 23:58:51 +0530

    testing

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      "/api":"https://deeblogapi.onrender.com"
-    }
-  },
   plugins: [react()],
 })
